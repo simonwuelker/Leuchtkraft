fb75d59598a8c0893cc1d736eeb430e25bd68996
Wuelle <simon.wuelker@arcor.de>
2021-07-04 15:11:22 +0200

    use regexes for lexing

--- src/interpreter.rs
+++ src/interpreter.rs
@@ -1,21 +1,39 @@
+use lazy_static::lazy_static;
+use regex::RegexSet;
+
 pub enum Token {
     Comment,
+    Variable(String),
+    Question,
+    Colon,
     EOF,
 }
 
-pub struct Interpreter {
-    /// The program code to be executed
+/// Converts the program into a stream of tokens
+pub struct Lexer {
     text: String,
+    pos: usize,
 }
 
-impl Interpreter {
+impl Lexer {
     pub fn new(text: String) -> Self {
         Self {
             text: text,
+            pos: 0,
         }
     }
 
-    pub fn next_token(&mut self) -> Token {
-        Token::EOF
+    pub fn read_tokens(&mut self) {
+        lazy_static! {
+            static ref set: RegexSet = RegexSet::new(&[
+                r"#.*\n", // Oneline Comments
+                r"[\w]+", // Variable Names
+                r"\?", // questions
+                r":", // Colons
+            ]).unwrap();
+        }
+        for m in set.matches(&self.text).iter() {
+            println!("{:?}", m.start);
+        }
     }
 }
--- src/main.rs
+++ src/main.rs
@@ -2,18 +2,12 @@ mod interpreter;
 
 use std::fs;
 use anyhow::Result;
-use interpreter::{Interpreter, Token};
+use interpreter::{Lexer, Token};
 
 fn main() -> Result<()> {
     let contents = fs::read_to_string("script.ap")?;
-    let mut i = Interpreter::new(contents);
-
-    loop {
-        match i.next_token() {
-            Token::Comment => {},
-            Token::EOF => break,
-        }
-    }
+    let mut i = Lexer::new(contents);
+    i.read_tokens();
 
     Ok(())
 }
